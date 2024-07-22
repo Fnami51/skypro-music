3d5c3226dcae992cd68777b1fbbb2a73d79b60fe
Fnami51 <fnm5160@gmail.com>
2024-07-22 10:50:53 +0300

    v6.3.2 - Complate playbar

--- skypro-music/components/bar.tsx
+++ skypro-music/components/bar.tsx
@@ -10,6 +10,10 @@ export default function Soundbar() {
   const [isPlaying, setIsPlaying] = useState(false);
   const { playlist } = useTracks();
   const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const [isLooping, setIsLooping] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [volume, setVolume] = useState(1);
 
   function togglePlay() {
     const audio = audioRef.current;
@@ -20,16 +24,42 @@ export default function Soundbar() {
     } else {
       audio.play();
     }
-    setIsPlaying((prev) => !prev);
+    setIsPlaying(!isPlaying);
   }
 
-  const handleEnded = () => {
-    if (currentTrackIndex < playlist.length - 1) {
+  function handleEnded() {
+    if (isLooping) {
+      audioRef.current?.play();
+    } else if (currentTrackIndex < playlist.length - 1) {
       setCurrentTrackIndex(currentTrackIndex + 1);
     } else {
       setCurrentTrackIndex(0);
     }
-  };
+  }
+
+  function handleTimeUpdate() {
+    setCurrentTime(audioRef.current?.currentTime ?? 0);
+  }
+
+  function handleLoadedMetadata() {
+    setDuration(audioRef.current?.duration ?? 0);
+  }
+
+  function handleVolumeChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const newVolume = Number(event.target.value);
+    setVolume(newVolume);
+    if (audioRef.current) {
+      audioRef.current.volume = newVolume;
+    }
+  }
+
+  function handleProgressChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const newTime = Number(event.target.value);
+    if (audioRef.current) {
+      audioRef.current.currentTime = newTime;
+    }
+    setCurrentTime(newTime);
+  }
 
   useEffect(() => {
     const audio = audioRef.current;
@@ -37,26 +67,43 @@ export default function Soundbar() {
     if (audio && playlist.length > 0) {
       audio.src = playlist[currentTrackIndex].track_file;
       audio.addEventListener('ended', handleEnded);
+      audio.addEventListener('timeupdate', handleTimeUpdate);
+      audio.addEventListener('loadedmetadata', handleLoadedMetadata);
 
-      audio.play();
+      if (isPlaying) {
+        audio.play();
+      }
 
       return () => {
         audio.removeEventListener('ended', handleEnded);
+        audio.removeEventListener('timeupdate', handleTimeUpdate);
+        audio.removeEventListener('loadedmetadata', handleLoadedMetadata);
       };
     }
-  }, [currentTrackIndex, playlist]);
+  }, [currentTrackIndex, playlist, isPlaying]);
 
   return (
     <div className={styles.background}>
       <div className={styles.content}>
         
-        <div className={styles.progress}></div>
+        <div>
+          <input
+            className={styles.progress}
+            type="range"
+            min="0"
+            max={duration}
+            value={currentTime}
+            onChange={handleProgressChange}
+          />
+          <span>{Math.floor(currentTime / 60)}:{Math.floor(currentTime % 60).toString().padStart(2, '0')}</span> / 
+          <span>{Math.floor(duration / 60)}:{Math.floor(duration % 60).toString().padStart(2, '0')}</span>
+        </div>
 
         <div className={styles.block}>
           <div className={styles.player}>
             <audio ref={audioRef} controls style={{ display: "none" }}></audio>
             <div className={styles.controls}>
-              <button className={styles.btnPrev}>
+              <button className={styles.btnPrev} onClick={() => setCurrentTrackIndex((prev) => (prev > 0 ? prev - 1 : playlist.length - 1))}>
                 <svg className={styles.btnPrevSvg}>
                   <use xlinkHref="img/icon/sprite.svg#icon-prev"></use>
                 </svg>
@@ -66,12 +113,12 @@ export default function Soundbar() {
                   <use xlinkHref="img/icon/sprite.svg#icon-play"></use>
                 </svg>
               </button>
-              <button className={styles.btnNext}>
+              <button className={styles.btnNext} onClick={() => setCurrentTrackIndex((prev) => (prev < playlist.length - 1 ? prev + 1 : 0))}>
                 <svg className={styles.btnNextSvg}>
                   <use xlinkHref="img/icon/sprite.svg#icon-next"></use>
                 </svg>
               </button>
-              <button className={classNames(styles.btnRepeat, styles.btnIcon)}>
+              <button className={classNames(styles.btnRepeat, styles.btnIcon)} onClick={() => setIsLooping(!isLooping)}>
                 <svg className={styles.btnRepeatSvg}>
                   <use xlinkHref="img/icon/sprite.svg#icon-repeat"></use>
                 </svg>
@@ -127,7 +174,11 @@ export default function Soundbar() {
                 <input
                   className={classNames(styles.volProgress, styles.btn)}
                   type="range"
-                  name="range"
+                  min="0"
+                  max="1"
+                  step="0.01"
+                  value={volume}
+                  onChange={handleVolumeChange}
                 />
               </div>
             </div>
