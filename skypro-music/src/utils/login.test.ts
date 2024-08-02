f1ed48a913155f03063e143f7321e2cbc9fb8052
Fnami51 <fnm5160@gmail.com>
2024-08-02 13:46:44 +0300

    v6.5.5 - Eeror test

--- skypro-music/src/utils/login.test.ts
+++ skypro-music/src/utils/login.test.ts
@@ -1,27 +1,7 @@
-/*
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import Home from "../app/login/page";
-import ReduxProvider from "../store/ReduxProvider";
-import { BrowserRouter as Router } from "react-router-dom";
-import '@testing-library/jest-dom';
+import renderer from "react-test-renderer";
+import Home from "../app/login/page"
 
-test('должен отображать ошибку при пустых полях и успешно входить при правильных данных', async () => {
-  render(
-    <ReduxProvider>
-        <Home />
-    </ReduxProvider>
-  );
-
-  // Проверяем отображение ошибок при пустых полях
-  fireEvent.click(screen.getByText('Войти'));
-  expect(await screen.findByText('Пожалуйста, заполните все поля.')).toBeInTheDocument();
-
-  // Заполняем поля и проверяем успешный вход
-  fireEvent.change(screen.getByPlaceholderText('Почта'), { target: { value: 'test@example.com' } });
-  fireEvent.change(screen.getByPlaceholderText('Пароль'), { target: { value: 'password' } });
-  
-  fireEvent.click(screen.getByText('Войти'));
-
-  // Здесь вы можете использовать Mock функции и API для проверки успешного входа
+it("renders correctly", () => {
+  const tree = renderer.create(<Home />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
-*/
